5777c0fc17717d344e245e2b35dec51c49713524
Tony Gaskell <thgaskell@gmail.com>
2018-10-27 10:12:42 -1000

    Move to demo folder

---
+++ demo/client-server/server.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+import * as p2pwn from "../src/lib/express-p2pwn";
+
+const app = express();
+
+p2pwn.initialize(app, { websocketEndpoint: "ws://localhost:8080" });
+
+app.listen(3000);
