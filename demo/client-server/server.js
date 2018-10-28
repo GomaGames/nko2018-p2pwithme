b48e8c21f0445934f6392afb5b908bd578643cbf
Tony Gaskell <thgaskell@gmail.com>
2018-10-28 12:20:19 -1000

    Set up localtunnel for middleware app

--- demo/client-server/server.js
+++ demo/client-server/server.js
@@ -4,16 +4,9 @@ const p2pwn = require("../../dist/lib/express-p2pwn");
 
 const app = express();
 
-const agent = p2pwn.initialize(app, {
-  websocketEndpoint: "ws://localhost:8080",
-  entry_url: "http://localhost:3001",
-  healthcheck_url: "http://localhost:3001",
+p2pwn.initialize(app, {
+  port: 3001,
   display_name: "Test Client Host"
 });
-
-console.debug("Updating the entry URL in 5 seconds");
-setTimeout(() => {
-  agent.setState({ entry_url: "ws://localhost:3001" });
-}, 5000);
 
 app.listen(3001);
--- src/lib/express-p2pwn/index.ts
+++ src/lib/express-p2pwn/index.ts
@@ -1,5 +1,8 @@
+import localtunnel from "localtunnel";
 import WebSocket from "ws";
 import { Application } from "express";
+
+import { HostRegistrationOffer } from "../../../types";
 
 import {
   ClientServerWebSocketMessage,
@@ -9,13 +12,13 @@ import {
 
 type AgentOptions = {
   websocketEndpoint: string;
-};
+} & HostRegistrationOffer;
 
 class P2PwnAgent {
   private socket: WebSocket;
   public readonly createdAt: Date | number;
 
-  constructor(options: AgentOptions & Partial<ClientHost>) {
+  constructor(options: AgentOptions) {
     this.createdAt = Date.now();
     this.socket = new WebSocket(options.websocketEndpoint);
 
@@ -58,15 +61,59 @@ class P2PwnAgent {
   }
 }
 
-export function initialize(
-  app: Application,
-  options: AgentOptions & Partial<ClientHost>
-): P2PwnAgent {
-  const agent = new P2PwnAgent(options);
+type Options = {
+  entry_url?: string;
+  port: number;
+  healthcheck_url?: string;
+  meta?: object;
+};
 
-  app.get("/p2pwn/stats", (req, res, next) => {
+export async function initialize(
+  app: Application,
+  options: Options
+): Application {
+  const meta = options.meta || {};
+  let entry_url = options.entry_url;
+  let healthcheck_url = options.healthcheck_url || entry_url;
+
+  let agent: P2PwnAgent;
+  if (entry_url && healthcheck_url) {
+    agent = new P2PwnAgent({
+      ...meta,
+      entry_url,
+      healthcheck_url,
+      websocketEndpoint: "wss://p2pwn-production.herokuapp.com"
+    });
+  } else if (typeof options.port === "number") {
+    await new Promise<P2PwnAgent>((resolve, reject) => {
+      localtunnel(options.port, (err, { url }) => {
+        if (err) {
+          reject(err);
+          return process.exit(1);
+        }
+        console.log(`Tunnel created to: ${url}`);
+
+        resolve(
+          new P2PwnAgent({
+            ...meta,
+            entry_url: url,
+            healthcheck_url: `${url}/_p2pwn/health`,
+            websocketEndpoint: "wss://p2pwn-production.herokuapp.com"
+          })
+        );
+      });
+    }).then(_agent => {
+      agent = _agent;
+    });
+  } else {
+    throw new Error("Please provide a port for P2Pwn to connect to");
+  }
+  app.get("/_p2pwn/health", (req, res) => {
+    res.sendStatus(200);
+  });
+  app.get("/_p2pwn/stats", (req, res, next) => {
     res.json(agent.stats);
   });
 
-  return agent;
+  return app;
 }
