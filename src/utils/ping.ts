c2756b61baee25d91e5e7dd0e1b9405dd0f2b355
Tony Gaskell <thgaskell@gmail.com>
2018-10-28 10:19:31 -1000

    Reject ping if status code greater than 400

--- src/utils/ping.ts
+++ src/utils/ping.ts
@@ -2,9 +2,9 @@ import request from "request";
 
 export default function ping(url: string) {
   return new Promise((resolve, reject) => {
-    request(url, function(err) {
-      if (err) {
-        return reject(err);
+    request(url, function(err, request) {
+      if (err || request.statusCode >= 400) {
+        return reject(err || request.statusCode);
       }
       resolve(true);
     });
